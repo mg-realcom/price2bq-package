b67e561892d393cb49e5b9fec05edb13115b3fd7
viktor <vi.dave@yandex.ru>
2023-05-13 14:17:03 +0300

    [other] add type hints

--- tests/test_file_prepare.py
+++ tests/test_file_prepare.py
@@ -1,22 +1,22 @@
 from src.price2bq_zfullio.main import prepare_novostroy_m, prepare_realty, prepare_cian, prepare_avito
 
 
-def test_realty():
+def test_realty() -> None:
     df = prepare_realty("test_realty.xls")
     assert len(df)
 
 
-def test_cian():
+def test_cian() -> None:
     df = prepare_cian("test_cian.xlsx")
     assert len(df) == 5
 
 
-def test_avito():
+def test_avito() -> None:
     df = prepare_avito("test_avito.csv")
     assert len(df) == 5
 
 
-def test_novostroy_m():
+def test_novostroy_m() -> None:
     df = prepare_novostroy_m("test_novostroy_m.xls")
     assert len(df)
 
